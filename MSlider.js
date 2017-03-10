41b63dd82515c414834da44c2a92307c7214df23
Manuel Valdez <valdezzmanuel88@gmail.com>
2017-03-09 23:17:43 -0800

    Add files via upload
    first commit of the project

---
+++ MSlider.js
@@ -0,0 +1,45 @@
+$(function(){
+  // get main slider
+  var MSlider = $(".MSlider");
+  var elementSize = {
+    width: "sizex",
+    height: "sizey"
+  }
+
+  // get slider
+  var Slides = $(".MSlider-item");
+
+  // set css style
+
+  // set events
+
+})
+
+
+/*
+ * elementSize => JSON that contains width and height of each slide
+ * type => Style of Slider
+ */
+function setCSSSlides(elementSize, type){
+  // setStyle
+}
+
+/*
+ * height => height of Slider
+ * padding => padding of each element in slider
+ * width => ...
+ * type => Style of slider
+ */
+function setCSSSlider(height, padding, width, type)
+{
+
+}
+
+// type #1
+var type_1 = {};
+
+// type #2
+var type_2 = {};
+
+// type #3
+var type_3 = {};
