8c8040d8a42f0d8787865bbe195bf80cb2cad866
Manuel Valdez <valdezzmanuel88@gmail.com>
2017-03-10 22:36:15 -0800

    Add files via upload
    JQuery initial configuration of library.

--- MSlider.js
+++ MSlider.js
@@ -1,45 +1,66 @@
 $(function(){
-  // get main slider
-  var MSlider = $(".MSlider");
-  var elementSize = {
-    width: "sizex",
-    height: "sizey"
+
+
+  $.fn.MSliding = function(params){
+
+    // size and style of the slider
+    var width = params.width?this:"100%",
+        height = params.height?this:"200px",
+        style_type = params.style_type?this:1;
+
+    // get main slider
+    var slider = $('.MSlider');
+    // get slides
+    var slides = $('.MSlider-item');
+
+    // wrap slides
+    slides.wrapAll("<div id='MSlider-content'/>");
+
+    // put arrows inside slider
+    $("#MSlider-content").before('<span id="left"><</span><span id="right">></span>');
+
+    // get arrows
+    var left = $('#MSlider-left');
+    var right = $('#MSlider-right');
+
+
+
+    /*
+     * elementSize => JSON that contains width and height of each slide
+     * type => Style of Slider
+     */
+    function setCSSSlides(elementSize, type){
+      // setStyle
+    }
+
+    /*
+     * height => height of Slider
+     * padding => padding of each element in slider
+     * width => ...
+     * type => Style of slider
+     */
+    function setCSSSlider(height, padding, width, type)
+    {
+
+    }
+
+    // type #1
+    var type_1 = {};
+
+    // type #2
+    var type_2 = {};
+
+    // type #3
+    var type_3 = {};
+
+
+
   }
 
-  // get slider
-  var Slides = $(".MSlider-item");
+  $('.MSlider').MSliding({
+    width: '100%',
+    height: '250px'
+  });
 
-  // set css style
-
-  // set events
 
 })
-
-
-/*
- * elementSize => JSON that contains width and height of each slide
- * type => Style of Slider
- */
-function setCSSSlides(elementSize, type){
-  // setStyle
-}
-
-/*
- * height => height of Slider
- * padding => padding of each element in slider
- * width => ...
- * type => Style of slider
- */
-function setCSSSlider(height, padding, width, type)
-{
-
-}
-
-// type #1
-var type_1 = {};
-
-// type #2
-var type_2 = {};
-
-// type #3
-var type_3 = {};
